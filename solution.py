159cda962259c71ded2ca18d27805abe398251bd
Griffin Riley <rileyg@oregonstate.edu>
2024-03-26 13:27:25 -0700

    completed part 2

--- solution.py
+++ solution.py
@@ -1,3 +1,6 @@
+from cgitb import small
+import time
+from functools import cache
 
 def solvepart1():
     #format data
@@ -12,7 +15,9 @@ def solvepart1():
     #find valid arrangements
     sum = 0
     for i in range(len(springsRows)):
-        sum = sum + checkAllArrangements(springsRows[i], [], recordsRows[i], 0)
+        val = checkAllArrangements(springsRows[i], [], recordsRows[i], 0)
+        sum = sum + val
+        # print("val: ", val)
     print(sum)
 
 #recursively checks all possible arrangements for a row of springs and returns the number which are valid
@@ -44,6 +49,58 @@ def checkIfRowValid(springs, record):
             lastRecordSpring = False
     return newRecord == record
 
+def solvepart2():
+    #format data
+    data = fileRead("input.txt")
+    smallSpringsRows = []
+    smallRecordsRows = []
+    for row in data:
+        splitRow = row.split(" ")
+        smallSpringsRows.append( splitRow[0] )
+        smallRecordsRows.append( tuple(splitRow[1].strip().split(",")) )
+
+    #multiply data by 5
+    springsRows = []
+    recordsRows = []
+    for i in range(len(smallSpringsRows)):
+        springsRows.append( "?".join([smallSpringsRows[i]] * 5) + ".")
+        recordsRows.append( tuple(smallRecordsRows[i] * 5) )
+    
+    #find valid arrangements
+    sum = 0
+    for i in range(len(springsRows)):
+        val = checkAllArrangmentsCaching(springsRows[i],recordsRows[i], 0)
+        sum = sum + val 
+    print(sum)
+
+#uses caching to recursively check all possible springs efficently
+#uses strings instead of arrays so caching can happen
+@cache
+def checkAllArrangmentsCaching(currentSpring, currentRecord, springRowLen):
+    if currentSpring == "":
+        if len(currentRecord) == 0 and springRowLen == 0:
+            return 1
+        else:
+            return 0
+
+    paths = []
+    if currentSpring[0] == "?":
+        paths = ["#","."]
+    else:
+        paths = currentSpring[0]
+    
+    sum = 0
+    for path in paths:
+        if path == "#":
+            sum = sum + checkAllArrangmentsCaching(currentSpring[1:], currentRecord, springRowLen + 1)
+        else:
+            if springRowLen > 0:
+                if len(currentRecord) > 0 and springRowLen == int(currentRecord[0]):
+                    sum = sum + checkAllArrangmentsCaching(currentSpring[1:], currentRecord[1:], 0)
+            else:
+                sum = sum + checkAllArrangmentsCaching(currentSpring[1:], currentRecord, 0)
+    return sum
+
 def fileRead(name):
     data = []
     f = open(name, "r")
@@ -51,4 +108,6 @@ def fileRead(name):
         data.append(line);
     return data
 
-solvepart1()
+start = time.time()
+solvepart2()
+print(time.time()-start)
